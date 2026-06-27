c112cfdbc42afbfa63dd4d9d5aa535cc2d7bc2e1
Isaac Braun <isaac@braunbauen.com>
2026-06-26 20:11:18 -0700

    chore: init cleanup after fork

--- lua/options.lua
+++ lua/options.lua
@@ -8,7 +8,11 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
 
 -- [[ Setting options ]]
 --  See `:help vim.o`
@@ -19,7 +23,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
