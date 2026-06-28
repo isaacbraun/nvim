4ca1b4da4f5c82e93bdd8f824ea35a923e6a5d37
Isaac Braun <isaac@braunbauen.com>
2026-06-28 11:17:43 -0700

    feat(theme): setup light/dark with catppuccin and gruvbox

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -6,17 +6,34 @@ local function gh(repo) return 'https://github.com/' .. repo end
 -- change the command under that to load whatever the name of that colorscheme is.
 --
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-vim.pack.add { gh 'catppuccin/nvim' }
+vim.pack.add {
+  gh 'catppuccin/nvim',
+  gh 'ellisonleao/gruvbox.nvim',
+}
+
 require('catppuccin').setup {
   background = {
     light = 'latte',
-    dark = 'mocha',
   },
 }
 
--- Load the colorscheme here.
--- Like many other themes, this one has different styles, and you could load
--- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-vim.cmd.colorscheme 'catppuccin'
+require('gruvbox').setup {
+  contrast = 'hard',
+}
+
+local function set_colorscheme()
+  if vim.o.background == 'light' then
+    vim.cmd.colorscheme 'catppuccin'
+  else
+    vim.cmd.colorscheme 'gruvbox'
+  end
+end
+
+set_colorscheme()
+
+vim.api.nvim_create_autocmd('OptionSet', {
+  pattern = 'background',
+  callback = set_colorscheme,
+})
 
 -- vim: ts=2 sts=2 sw=2 et
