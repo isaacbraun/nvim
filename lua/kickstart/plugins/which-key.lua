8a84f4148f832b3d15fe9a6609ba8c5719a19163
Isaac Braun <isaac@braunbauen.com>
2026-06-28 20:06:05 -0700

    feat(which-key): add Git group

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -4,12 +4,13 @@ local function gh(repo) return 'https://github.com/' .. repo end
 vim.pack.add { gh 'folke/which-key.nvim' }
 require('which-key').setup {
   -- Delay between pressing a key and opening which-key (milliseconds)
-  delay = 0,
+  delay = 10,
   icons = { mappings = vim.g.have_nerd_font },
   -- Document existing key chains
   spec = {
     { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
     { '<leader>t', group = '[T]oggle' },
+    { '<leader>g', group = '[G]it', mode = { 'n' } },
     { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
     { 'gr', group = 'LSP Actions', mode = { 'n' } },
   },
