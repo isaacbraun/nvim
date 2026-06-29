775b75a2449d967008b2c7150841c35cb6d17b3e
Isaac Braun <isaac@braunbauen.com>
2026-06-28 22:27:02 -0700

    feat(git): add rhubarb plugin

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,3 +1,10 @@
-vim.pack.add { 'https://github.com/tpope/vim-fugitive' }
+vim.g.github_enterprise_urls = { 'https://devtopia.esri.com' }
+
+vim.pack.add {
+  'https://github.com/tpope/vim-fugitive',
+  'https://github.com/tpope/vim-rhubarb',
+}
+
+vim.api.nvim_create_user_command('Browse', function(opts) vim.fn.system { 'open', opts.fargs[1] } end, { nargs = 1 })
 
 vim.keymap.set('n', '<leader>gs', '<cmd>vertical Git | vertical resize 60<cr>', { desc = '[G]it [S]tatus' })
