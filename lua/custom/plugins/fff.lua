e87889cd995f1b1c3ca0ab1da838b19d06b53ba8
Isaac Braun <isaac@braunbauen.com>
2026-06-30 17:23:45 -0700

    feat(fff): fix Catppucin grep highlighting and cleanup

--- lua/custom/plugins/fff.lua
+++ lua/custom/plugins/fff.lua
@@ -10,26 +10,26 @@ vim.api.nvim_create_autocmd('PackChanged', {
   end,
 })
 
+-- Create an empty highlight group to counteract Catppuccin's CursorLine conflicting highlight group
+vim.api.nvim_set_hl(0, 'FFFCursorLineNull', {})
+
 vim.g.fff = {
   lazy_sync = true,
+  prompt_vim_mode = true,
+  keymaps = {
+    focus_preview = '<leader>p',
+  },
+  layout = {
+    border = 'rounded',
+  },
+  hl = {
+    winhl = {
+      preview = 'CursorLine:FFFCursorLineNull',
+    },
+  },
 }
 
-local download = require 'fff.download'
-if not vim.uv.fs_stat(download.get_binary_path()) then download.download_or_build_binary() end
-
-local ok, fff = pcall(require, 'fff')
-if ok then
-  fff.setup {
-    prompt_vim_mode = true,
-    keymaps = {
-      focus_preview = "<leader>p"
-    }
-  }
-else
-  vim.notify('Failed to load fff.nvim: ' .. fff, vim.log.levels.ERROR)
-end
-
+local fff = require 'fff'
 vim.keymap.set('n', '<leader>sf', fff.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sg', fff.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set({ 'n', 'v' }, '<leader>sw', function() fff.live_grep { query = vim.fn.expand '<cword>' } end, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', fff.live_grep, { desc = '[S]earch by [G]rep' })
