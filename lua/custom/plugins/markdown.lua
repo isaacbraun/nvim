663373e3a785bace017cc006d70a55cb3d2ba1c2
Isaac Braun <isaac@braunbauen.com>
2026-07-02 21:25:50 -0700

    feat(markdown): add render markdown plugin

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,9 @@
+vim.pack.add {
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-mini/mini.nvim',
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+}
+
+require('render-markdown').setup {
+  completions = { lsp = { enabled = true } },
+}
