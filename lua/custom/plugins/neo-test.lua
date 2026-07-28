794c3f6bee1d53fc8e30ad9001c0f1541f83be58
Isaac Braun <isaac@braunbauen.com>
2026-07-27 18:41:05 -0700

    feat(neotest): add browser test specific keymap

--- lua/custom/plugins/neo-test.lua
+++ lua/custom/plugins/neo-test.lua
@@ -34,6 +34,18 @@ neotest.setup(config)
 
 vim.keymap.set('n', '<leader>nt', function() neotest.summary.toggle() end, { desc = 'Toggle [N]eo[T]est summary window' })
 vim.keymap.set('n', '<leader>nr', function() neotest.run.run() end, { desc = '[N]eotest [R]un nearest test' })
+vim.keymap.set(
+  'n',
+  '<leader>nb',
+  function()
+    neotest.run.run {
+      nil,
+      { env = { BROWSER_TESTS = 'true' } },
+      suite = false,
+    }
+  end,
+  { desc = '[N]eotest run [B]rowser tests' }
+)
 vim.keymap.set('n', '<leader>nf', function() neotest.run.run(vim.fn.expand '%') end, { desc = '[N]eotest run [F]ile' })
 vim.keymap.set('n', '<leader>no', function() neotest.output_panel.toggle() end, { desc = 'Toggle [N]eotest [O]utput panel' })
 vim.keymap.set('n', '<leader>ns', function() neotest.run.stop() end, { desc = '[N]eotest [S]top' })
