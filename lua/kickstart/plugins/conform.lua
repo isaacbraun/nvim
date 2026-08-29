0e98476e3ce7b2362884fbbc026a9dc4f9ed7e48
Isaac Braun <isaac@braunbauen.com>
2026-08-28 21:23:28 -0700

    feat(conform): add css and go formatters

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -26,12 +26,14 @@ require('conform').setup {
     -- python = { "isort", "black" },
     --
     -- You can use 'stop_after_first' to run the first available formatter from the list
+    go = { 'gofmt', 'goimports' },
     astro = { 'prettierd', 'prettier', stop_after_first = true },
     javascript = { 'prettierd', 'prettier', stop_after_first = true },
     typescript = { 'prettierd', 'prettier', stop_after_first = true },
     typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
     html = { 'prettierd', 'prettier', stop_after_first = true },
     mdx = { 'prettierd', 'prettier', stop_after_first = true },
+    css = { 'prettierd', 'prettier', stop_after_first = true },
   },
 }
 
